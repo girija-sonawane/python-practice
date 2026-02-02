a6453c2224bdc07371d3d1ce1d88be7b14526b0e
girija-sonawane <sonawanegirija0@gmail.com>
2026-02-02 20:55:18 +0530

    Added Day 9 file handling programs

---
+++ day9_write_file.py
@@ -0,0 +1,8 @@
+file = open("data.txt", "w")
+
+file.write("Hello, this file is created using Python.\n")
+file.write("I am learning file handling step by step.")
+
+file.close()
+
+print("Data written to file successfully")
